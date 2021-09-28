323ba363cdd201c856e492760cb96fdcc5d682a6
anildegirmenci <anildegirmenci71@gmail.com>
2021-09-28 21:21:53 +0300

    Some improvements have been made.

--- App.js
+++ App.js
@@ -7,8 +7,6 @@ export default function App() {
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
       }}
     >
       <Home />
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Platform } from 'react-native'
+import Categories from '../components/Categories'
 import HeaderTabs from '../components/HeaderTabs'
+import SearchBar from '../components/SearchBar'
 
 export default function Home() {
     return (
-        <SafeAreaView>
-            <HeaderTabs />
+        <SafeAreaView style={{backgroundColor:'#eee',flex:1}}>
+            <View style={{backgroundColor:'#fff', 
+                padding:15, paddingTop: Platform.OS === 'android' ? 55 : 15}}>
+                <HeaderTabs />
+                <SearchBar />
+            </View>
+            <Categories />
         </SafeAreaView>
     )
 }
