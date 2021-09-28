db750ce11893f9043a85e9bba9e7d6488a7b12b6
anildegirmenci <anildegirmenci71@gmail.com>
2021-09-29 01:23:23 +0300

    Components are revised.

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import { View, Text, SafeAreaView, Platform } from 'react-native'
-import Categories from '../components/Categories'
-import HeaderTabs from '../components/HeaderTabs'
-import SearchBar from '../components/SearchBar'
+import React from 'react';
+import { View, Text, SafeAreaView, Platform, ScrollView } from 'react-native';
+import Categories from '../components/Categories';
+import HeaderTabs from '../components/HeaderTabs';
+import SearchBar from '../components/SearchBar';
+import RestaurantItem from '../components/RestaurantItem';
+
 
 export default function Home() {
     return (
-        <SafeAreaView style={{backgroundColor:'#eee',flex:1}}>
-            <View style={{backgroundColor:'#fff', 
-                padding:15, paddingTop: Platform.OS === 'android' ? 55 : 15}}>
+        <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
+            <View style={{
+                backgroundColor: '#fff',
+                padding: 15, paddingTop: Platform.OS === 'android' ? 55 : 15
+            }}>
                 <HeaderTabs />
                 <SearchBar />
             </View>
-            <Categories />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Categories />
+                <RestaurantItem />
+            </ScrollView>
         </SafeAreaView>
     )
 }
